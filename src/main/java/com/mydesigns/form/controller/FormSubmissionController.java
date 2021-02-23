3e8b315f6cedd2e6aee6f5f0e5d43c77112de34e
Jeff Haupt <jhaupt@southbendin.gov>
2021-02-23 16:31:38 -0500

    create Thymeleaf template and controller

--- src/main/java/com/mydesigns/form/controller/FormSubmissionController.java
+++ src/main/java/com/mydesigns/form/controller/FormSubmissionController.java
@@ -1,4 +1,25 @@
 package com.mydesigns.form.controller;
 
+
+import com.mydesigns.form.entity.FormSubmissionFields;
+import com.mydesigns.form.service.FormSubmissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/submitForm")
 public class FormSubmissionController {
+
+    @Autowired
+    FormSubmissionService service;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String createFormSubmission(FormSubmissionFields formSubmit)
+    {
+        service.createFormSubmission(formSubmit);
+        return "redirect:/";
+    }
+
 }
